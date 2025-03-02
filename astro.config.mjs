0006b10348d58df2249ccf6ec86aaebca30e620c
Max Eliseev <mxseev@gmail.com>
2025-03-03 02:17:41 +0500

    fix: hardcode env vars))

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import {defineConfig, envField} from "astro/config"
+import {defineConfig} from "astro/config"
 import node from "@astrojs/node"
 import sitemap from "@astrojs/sitemap"
 import mdx from "@astrojs/mdx"
@@ -13,12 +13,5 @@ export default defineConfig({
   i18n: {
     locales: ["ru", "en"],
     defaultLocale: "ru"
-  },
-  env: {
-    schema: {
-      UMAMI_HOST: envField.string({context: "server", access: "public", optional: true, default: undefined}),
-      UMAMI_WEBSITE_ID: envField.string({context: "server", access: "public", optional: true, default: undefined}),
-      LOOKING_FOR_JOB: envField.boolean({context: "server", access: "public", optional: true, default: true})
-    }
   }
 })
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,27 +1,28 @@
+import {faEnvelope, faFile} from "@fortawesome/free-solid-svg-icons"
 import {
   faGithub,
   faTelegram,
   faXTwitter,
-  type IconDefinition,
-} from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
-import { LOOKING_FOR_JOB } from "astro:env/server";
+  type IconDefinition
+} from "@fortawesome/free-brands-svg-icons"
+
+const LOOKING_FOR_JOB = false
 
 export type Link = {
-  title: string;
-  href: string;
-  icon: IconDefinition;
-};
-
-const links: Link[] = [
-  { title: "X.com", href: "https://x.com/mxseev", icon: faXTwitter },
-  { title: "Github", href: "https://github.com/mxseev", icon: faGithub },
-  { title: "Telegram", href: "https://t.me/mxseev", icon: faTelegram },
-  { title: "Email", href: "mailto:mxseev@gmail.com", icon: faEnvelope },
-];
-
-if (LOOKING_FOR_JOB) {
-  links.push({ title: "CV", href: "/cv", icon: faFile });
+  title: string
+  href: string
+  icon: IconDefinition
 }
 
-export { links };
+const links: Link[] = [
+  {title: "X.com", href: "https://x.com/mxseev", icon: faXTwitter},
+  {title: "Github", href: "https://github.com/mxseev", icon: faGithub},
+  {title: "Telegram", href: "https://t.me/mxseev", icon: faTelegram},
+  {title: "Email", href: "mailto:mxseev@gmail.com", icon: faEnvelope}
+]
+
+if (LOOKING_FOR_JOB) {
+  links.push({title: "CV", href: "/cv", icon: faFile})
+}
+
+export {links}
